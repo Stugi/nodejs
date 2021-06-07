aed33be6afcfa66d5f532bc52dc75d3829781781
Arina <anirak.anirak@mail.ru>
2021-06-07 23:12:32 +0300

    +

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,12 +1,36 @@
 let weights = [3,15,52,12,52];
 console.log(anniguliruyEto(weights));
 function anniguliruyEto(array){
-	while(array.length > 1){
-		array.sort((e1,e2)=>e1-e2);		
-		let maxItems = array.splice(array.length-2);
-		let newItem = maxItems[1]-maxItems[0];
-		if(newItem > 0)
-			array.push(newItem);
+	if(array.length <=1){
+		return (array[0])?array[0]:0;
 	}
-	return (array[0])?array[0]:0;
+
+	while(true){
+		let max1 = array[0], 
+			max2 = array[1], 
+			ind1 = 0, 
+			ind2 = 1;
+		for(let i = 2; i<array.length; i++){			
+			if(array[i]>max1){
+				max1 = array[i];
+				ind1 = i;
+			} else if(array[i]>max2){
+				max2 = array[i];
+				ind2 = i;
+			}
+		}
+				
+		if(max2>max1){
+			max1,max2=(max2,max1);
+			ind1,ind2=(ind2,ind1);
+		}
+		
+		if(max2===0){
+			return max1;
+		}
+		
+		array[ind1] = max1-max2;
+		array[ind2] = 0;
+	}
+	
 }
