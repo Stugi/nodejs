de24c9b5acc059a7b984bd8259037ffda550cc87
Arina <anirak.anirak@mail.ru>
2021-06-07 21:05:12 +0300

    first commit

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,17 +1,12 @@
-let weights = [26565,54654,45454,12,52];
+let weights = [3,15,52,12,52];
 console.log(anniguliruyEto(weights));
 function anniguliruyEto(array){
 	while(array.length > 1){
-		array.sort((e1,e2)=>e1-e2);
-		let r = getRandomNullOrOne();
+		array.sort((e1,e2)=>e1-e2);		
 		let maxItems = array.splice(array.length-2);
-		let newItem = maxItems[r]*(-1)+maxItems[r===0?1:0];
+		let newItem = maxItems[1]-maxItems[0];
 		if(newItem > 0)
-			array.push(newItem);		
+			array.push(newItem);
 	}
-	return array[0];
-}
-
-function getRandomNullOrOne(){
-	return Math.floor(Math.random() * 1);
+	return (array[0])?array[0]:0;
 }
